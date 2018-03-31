d652a95094f1ab0de78efc8b11412d4e2e463ad9
foo <bar@example.com>
2018-03-31 18:51:33 +0530

    Automating Compilation of the Linux Kernel - 1.0

---
+++ Compiling Linux Kernels/Automate LKC.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "This is a script to Automate the process of compiling any Linux Kernel."
+echo "This script assumes you have the required .tar.gz or .tar.xz file of the Kernel you wish to compile"
+echo "Paste this script in the directory you have the tarball archive, and run it from your bash."
+echo "Happy Compiling! :)"
